00c2a0536e9cb02075dbebd879d186a5ab8981c5
vikash <vikashraj144@gmail.com>
2018-05-15 19:55:44 +0530

    user module updated

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProfileComponent]
+  declarations: [ProfileComponent, UserComponent]
 })
 export class UserModule { }
