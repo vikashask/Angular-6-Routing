af8268b05051ddd71bbaf901f34205cea4ce9dbe
vikash <vikashraj144@gmail.com>
2018-05-20 15:32:13 +0530

    app routing update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,16 @@
 import { Route, Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { UserModule } from './user/user.module';
 
 const appRoutes:Routes = [
-    {path:'',redirectTo:'',pathMatch:'full'},
-    {path:'home',component:HomeComponent}
+    {path:'',redirectTo:'/home',pathMatch:'full'},
+    {path:'home',component:HomeComponent},
+    {
+        path:'users',
+        loadChildren: () => UserModule
+    },
+    {path:'**',component:NotFoundComponent}
 ];
 
 export const AppRouting = RouterModule.forRoot(appRoutes,{
