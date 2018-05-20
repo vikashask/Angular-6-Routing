5152771d6be1e65bee7dbbcd560bb51ff22ae6e1
vikash <vikashraj144@gmail.com>
2018-05-20 15:32:04 +0530

    user moduke updated

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { UserComponent } from './user/user.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const userRoutes:Routes = [
+  {path : '',component:UserComponent},
+  {path:'profile',component:ProfileComponent,pathMatch:'full'}
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userRoutes)
   ],
   declarations: [ProfileComponent, UserComponent]
 })
